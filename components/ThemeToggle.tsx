34b8e368182aa5f66ae7531cfe3206cd0c62293c
Ankit Raj <147795844+lucifer5094@users.noreply.github.com>
2025-02-26 20:37:40 +0530

    Updated themetoggle.tsx

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,28 +1,27 @@
-"use client";
-
-import { useState, useEffect } from "react";
+'use client';
+// import { useTheme } from '@/context/ThemeContext';
+import { useEffect, useState } from 'react';
 
 export default function ThemeToggle() {
+  // const { isDark, toggleTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    setMounted(true); // Component mounted on client
-    const storedTheme = localStorage.getItem("theme") || "light";
-    setTheme(storedTheme);
+    setMounted(true); 
   }, []);
 
-  if (!mounted) return null; // Prevent hydration error
+  if (!mounted) return null; 
 
   return (
-    <button
-      onClick={() => {
-        const newTheme = theme === "light" ? "dark" : "light";
-        setTheme(newTheme);
-        localStorage.setItem("theme", newTheme);
-      }}
-    >
-      {theme === "light" ? "🌞" : "🌙"}
-    </button>
+    <div></div>
+    // <button
+    //   onClick={() => {
+    //     console.log("Theme toggled. isDark:", !isDark); // Debugging
+    //     toggleTheme();
+    //   }}
+    //   className="fixed bottom-5  rounded-full border theme-toggle-btn  transition duration-300"
+    // >
+    //   {isDark ? "🌙 Dark Mode" : "🌞 Light Mode"}
+    // </button>
   );
 }
